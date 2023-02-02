ec7eaf93f1a1d25dbcf8105c8ce3a666d63af0af
PACMAN-3\wenyo <wenyong.lim@icloud.com>
2023-02-02 15:37:46 +0000

    Refactoring/ renaming 06-Modal

--- learning-code-files/06-Modal-UI/script.js
+++ learning-code-files/06-Modal-UI/script.js
@@ -4,29 +4,29 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
+// Closing and Opening Functions
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
 const openModal = function () {
-  // console.log('button clicked');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 
-for (var i = 0; i < btnsOpenModal.length; i++) {
+// Event handle for each modal button
+for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener('click', openModal);
   btnCloseModal.addEventListener('click', closeModal);
 }
 
+// Exit modal on clicking on overlay
 overlay.addEventListener('click', closeModal);
 
+// Exit modal on pressing Esc Key
 document.addEventListener('keydown', function (e) {
-  // console.log(e.key)
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
 });
-
-console.log('Cheng is cool');
